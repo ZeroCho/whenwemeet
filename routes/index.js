f479b8533e689bb2ab596b93947fde0001c578c3
Zero Cho <zerohch0@gmail.com>
2015-08-03 16:47:34 +0900

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -5,27 +5,34 @@ module.exports = function (passport) {
 	router.get('/', function(req, res, next) {
 		if (req.user) {
 			res.render('index', {
-				title: '우리언제만나'
+				title: '우리언제만나',
+				user: req.user
 			});
 		} else {
-			res.render('login');
+			res.render('login', {
+				title: '로그인::우리언제만나'
+			});
 		}
 	});
 	router.get('/login/kakao', passport.authenticate('kakao'));
 	router.get('/oauth/kakao', passport.authenticate('kakao', {
 	    failureRedirect: '/'
 	}), function (req, res) {
-		res.send('success');
+		console.log('kakao login success!');
+		res.redirect('/');
 	});
 	router.get('/login/facebook', passport.authenticate('facebook'));
 	router.get('/oauth/facebook', passport.authenticate('facebook', {
 	    failureRedirect: '/'
 	}), function (req, res) {
-		res.send('success');
+		console.log('facebook login success!');
+		res.redirect('/');
 	});
 	router.get('/logout', function(req, res){
-	  req.logout();
-	  res.send('success');
+		req.session.destroy()
+	  	req.logout();
+	  	console.log('logged out!')
+	  	res.redirect('/');
 	});
 	return router;
 };
