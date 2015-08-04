b545279d6367a7e83b861c4676aef70ed5924d0d
Zero Cho <zerohch0@gmail.com>
2015-08-04 14:46:00 +0900

    Update wwm.modal.js

--- public/js/wwm.modal.js
+++ public/js/wwm.modal.js
@@ -8,15 +8,40 @@ wwm.modal = (function (){
 		setJqMap($target);
 		jqMap.$close.click(oncloseModal);
 		stMap.$modal.show();
+		jqMap.$createRoom.click(createRoom);
 	}
 	function setJqMap($con) {
 		jqMap = {
 			$con: $con,
-			$close: $con.find('.modal-close')
+			$close: $con.find('.modal-close'),
+			$title: $con.find('#room-title'),
+			$number: $con.find('#room-number'),
+			$password: $con.find('#room-password'),
+			$createRoom: $con.find('#create-room-btn')
 		};
 	}
 	function onCloseModal() {
 		stMap.$modal.hide();
+	}
+	function createRoom() {
+		var data;
+		var title = jqMap.$title.val();
+		var number = jqMap.$number.val();
+		var password = jqMap.$password.val();
+		if (!title) {
+			alert('제목을 입력하세요.');
+			return;
+		}
+		if (!number) {
+			alert('인원수를 선택하세요!');
+			return;
+		}
+		data = {
+			title: jqMap.$title.val(),
+			number: jqMap.$number.val(),
+			password: jqMap.$password.val()
+		}
+		wwm.model.createRoom(data);
 	}
 	return {
 		initModule: initModule
