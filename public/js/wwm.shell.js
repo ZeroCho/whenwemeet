f893a71832dc7d41ffa711d915a377f5354046ec
Zero Cho <zerohch0@gmail.com>
2015-08-04 12:16:36 +0900

    Update wwm.shell.js

--- public/js/wwm.shell.js
+++ public/js/wwm.shell.js
@@ -12,7 +12,8 @@ wwm.shell = (function () {
 		console.log('login: ' + localStorage.login);
 		console.log('first: ' + localStorage.first)
 		var logged = localStorage.login && !!JSON.parse(localStorage.login);
-		var first = localStorage.first && JSON.parse(localStorage.first);
+		var first = localStorage.first && JSON.parse(localStorage.first);\
+		setJqMap($con);
 		//if (first) {
 		//	wwm.modal.initModule($('#wwm-intro').html());
 		//}
