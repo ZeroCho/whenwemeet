449ea34b9ab9363103544c89abfa0d384d9eac0c
Zero Cho <zerohch0@gmail.com>
2015-08-04 15:29:11 +0900

    Update wwm.room.js

--- public/js/wwm.room.js
+++ public/js/wwm.room.js
@@ -5,7 +5,9 @@ wwm.room = (function(){
     jqMap = {
       $con: $con,
       $explode: $con.find('#explode-room'),
-      $changeTitle: $con.find('#change-title)
+      $ban: $con.find('#ban-people-btn'),
+      $changeNumber: $con.find('#change-number-btn'),
+      $changeTitle: $con.find('#change-room-title')
     };
   }
   function initModule($con) {
