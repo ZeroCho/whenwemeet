4f6911b5622ca781a9fb82346b8dbecd8d14a01a
ZeroCho <zerohch0@gmail.com>
2015-08-08 20:02:18 +0900

    add console to index.js

--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,30 @@
 var express = require('express');
 var router = express.Router();
-var pg = require('pg');
-
+var Pgb = require('pg-bluebird');
+console.log('DATAURL: ' + process.env.DATABASE_URL);
+var pgb = new Pgb().connect(process.env.DATABASE_URL);
+pgb.catch(function(err) {
+	console.log('app.js::pgb ' + err);
+});
+var cnn;
 module.exports = function () {
 	router.get('/', function(req, res) {
 		res.render('index', {
 			title: '우리언제만나',
 			user: req.user
-		});		
+		});
 	});
 	router.get('/member/:id', function (req, res) {
 		var id = req.params.id;
-		pg.connect(process.env.DATABASE_URL, function (err, client, done) {
-			if (err) {
-				return console.error('error fetching client from pool', err);
-			}
-			client.query('SELECT * FROM members WHERE id = ' + id, function (err, result) {
-				done();
-				if (err) {
-					return console.error('error running query', err);
-				}
-				res.send(result.rows);
-			});
+		pgb.then(function (connection) {
+			cnn = connection;
+			return cnn.client.query(
+				'SELECT * FROM members WHERE id = ' + id
+			);
+		}).then(function (result) {
+			res.send(result);
+		}).catch(function (err) {
+			console.log('member' + err);
 		});
 	});
 	router.post('/room/:name', function (req, res) {
@@ -30,49 +33,41 @@ module.exports = function () {
 		var title = req.body.title;
 		var number = req.body.number || 2;
 		var password = req.body.password || null;
-		pg.connect(process.env.DATABASE_URL, function (err, client, done) {
-			if (err) {
-				return console.error('error fetching client from pool', err);
-			}
-			client.query(
+		pgb.then(function (connection) {
+			cnn = connection;
+			return cnn.client.query(
 				'INSERT INTO rooms (id, maker, title, number, password) ' +
-				'VALUES (' + id + ', ' + maker + ',' + title + ',' + number + ',' + password + ')',
-			function(err, result) {
-				done();
-				if (err) {
-					return console.error('error running query', err);
-				}
-				res.send(result.rows);
-			});
+				'VALUES (' + id + ', ' + maker + ',' + title + ',' + number + ',' + password + ')'
+			);
+		}).then(function (result) {
+			res.send(result);
+		}).catch(function (err) {
+			console.log('room ' + err);
 		});
 	});
 	router.get('/rooms', function (req, res) {
-		pg.connect(process.env.DATABASE_URL, function (err, client, done) {
-			if (err) {
-				return console.error('error fetching client from pool', err);
-			}
-			client.query('SELECT * FROM rooms', function(err, result) {
-				done();
-				if (err) {
-					return console.error('error running query', err);
-				}
-				res.send(result.rows);
-			});
+		pgb.then(function (connection) {
+			cnn = connection;
+			return cnn.client.query(
+				'SELECT * FROM rooms'
+			);
+		}).then(function (result) {
+			res.send(result);
+		}).catch(function (err) {
+			console.log('rooms ' + err);
 		});
 	});
 	router.get('/rooms/:query', function (req, res) {
 		var query = req.params.query;
-		pg.connect(process.env.DATABASE_URL, function (err, client, done) {
-			if (err) {
-				return console.error('error fetching client from pool', err);
-			}
-			client.query('SELECT * FROM rooms where title = ' + query, function(err, result) {
-				done();
-				if (err) {
-					return console.error('error running query', err);
-				}
-				res.send(result.rows);
-			});
+		pgb.then(function (connection) {
+			cnn = connection;
+			return cnn.client.query(
+				'SELECT * FROM rooms where title = ' + query
+			);
+		}).then(function (result) {
+			res.send(result);
+		}).catch(function (err) {
+			console.log('roomsq ' + err);
 		});
 	});
 	return router;
