264f21b019044adf7f97184ab5aefa289eb148c4
ZeroCho <zerohch0@gmail.com>
2015-08-09 12:31:07 +0900

    change index.js

--- routes/index.js
+++ routes/index.js
@@ -2,73 +2,77 @@ var express = require('express');
 var router = express.Router();
 var Pgb = require('pg-bluebird');
 var pgb = new Pgb();
-
 var cnn;
-module.exports = function () {
-	router.get('/', function(req, res) {
-		res.render('index', {
-			title: '우리언제만나',
-			user: req.user
-		});
+router.get('/', function(req, res) {
+	res.render('index', {
+		title: '우리언제만나',
+		user: req.user
 	});
-	router.get('/member/:id', function (req, res) {
-		var id = req.params.id;
-		pgb.connect(process.env.HEROKU_POSTGRESQL_AMBER_URL).then(function (connection) {
-			cnn = connection;
-			return cnn.client.query(
-				'SELECT * FROM members WHERE id = ' + id
-			);
-		}).then(function (result) {
-			res.send(result);
-		}).catch(function (err) {
-			console.log('member' + err);
-		});
+});
+router.get('/main', function (req, res) {
+	res.render('index', {
+		title: '우리언제만나',
+		user: req.user
 	});
-	router.post('/room/:name', function (req, res) {
-		var id = req.params.name;
-		var maker = req.body.maker;
-		var title = req.body.title;
-		var number = req.body.number || 2;
-		var password = req.body.password || null;
-		pgb.connect(process.env.HEROKU_POSTGRESQL_AMBER_URL).then(function (connection) {
-			cnn = connection;
-			return cnn.client.query(
-				'INSERT INTO rooms (id, maker, title, number, password) ' +
-				'VALUES (' + id + ', ' + maker + ',' + title + ',' + number + ',' + password + ')'
-			);
-		}).then(function (result) {
-			res.send(result);
-		}).catch(function (err) {
-			console.log('room ' + err);
-		});
+})
+router.get('/member/:id', function (req, res) {
+	var id = req.params.id;
+	pgb.connect(process.env.HEROKU_POSTGRESQL_AMBER_URL).then(function (connection) {
+		cnn = connection;
+		return cnn.client.query(
+			'SELECT * FROM members WHERE id = ' + id
+		);
+	}).then(function (result) {
+		res.send(result);
+	}).catch(function (err) {
+		console.log('member' + err);
 	});
-	router.get('/rooms', function (req, res) {
-		console.log('rooms' + JSON.stringify(req));
-		pgb.connect(process.env.HEROKU_POSTGRESQL_AMBER_URL).then(function (connection) {
-			console.log('connected');
-			cnn = connection;
-			return cnn.client.query(
-				'SELECT * FROM rooms'
-			);
-		}).then(function (result) {
-			console.log('result');
-			res.send(result);
-		}).catch(function (err) {
-			console.log('rooms ' + err);
-		});
+});
+router.post('/room/:name', function (req, res) {
+	var id = req.params.name;
+	var maker = req.body.maker;
+	var title = req.body.title;
+	var number = req.body.number || 2;
+	var password = req.body.password || null;
+	pgb.connect(process.env.HEROKU_POSTGRESQL_AMBER_URL).then(function (connection) {
+		cnn = connection;
+		return cnn.client.query(
+			'INSERT INTO rooms (id, maker, title, number, password) ' +
+			'VALUES (' + id + ', ' + maker + ',' + title + ',' + number + ',' + password + ')'
+		);
+	}).then(function (result) {
+		res.send(result);
+	}).catch(function (err) {
+		console.log('room ' + err);
 	});
-	router.get('/rooms/:query', function (req, res) {
-		var query = req.params.query;
-		pgb.connect(process.env.HEROKU_POSTGRESQL_AMBER_URL).then(function (connection) {
-			cnn = connection;
-			return cnn.client.query(
-				'SELECT * FROM rooms where title = ' + query
-			);
-		}).then(function (result) {
-			res.send(result);
-		}).catch(function (err) {
-			console.log('roomsq ' + err);
-		});
+});
+router.get('/rooms', function (req, res) {
+	console.log('rooms' + JSON.stringify(req));
+	pgb.connect(process.env.HEROKU_POSTGRESQL_AMBER_URL).then(function (connection) {
+		console.log('connected');
+		cnn = connection;
+		return cnn.client.query(
+			'SELECT * FROM rooms'
+		);
+	}).then(function (result) {
+		console.log('result');
+		res.send(result);
+	}).catch(function (err) {
+		console.log('rooms ' + err);
 	});
-	return router;
-};
+});
+router.get('/rooms/:query', function (req, res) {
+	var query = req.params.query;
+	pgb.connect(process.env.HEROKU_POSTGRESQL_AMBER_URL).then(function (connection) {
+		cnn = connection;
+		return cnn.client.query(
+			'SELECT * FROM rooms where title = ' + query
+		);
+	}).then(function (result) {
+		res.send(result);
+	}).catch(function (err) {
+		console.log('roomsq ' + err);
+	});
+});
+
+module.exports = router;
