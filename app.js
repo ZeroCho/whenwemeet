9fdcb87287f78af2a7474af39963cd84a1537e07
ZeroCho <zerohch0@gmail.com>
2015-08-09 12:09:46 +0900

    install newrelic

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
 var adaro = require('adaro');
+require('newrelic');
 
 var app = express();
 
