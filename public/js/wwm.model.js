57284f9d28720edc8e2a75872ba66b7824bde54d
Zero Cho <zerohch0@gmail.com>
2015-08-11 13:41:16 +0900

    Update wwm.model.js

--- public/js/wwm.model.js
+++ public/js/wwm.model.js
@@ -27,17 +27,54 @@ wwm.model = (function () {
 		return deferred.promise();
 	}
 	function ban(id, rid) {
-		$.post('/ban/' + id, {rid: rid});
+		var deferred = $.Deferred();
+		$.post('/ban/' + id, {rid: rid}).done(function(res) {
+			console.log(res);
+			deferred.resolve(res);
+		}).fail(function(err) {
+			console.log(err);
+			deferred.reject(err);
+		});
+		return deferred.promise();
 	}
 	function changeTitle(rid, title) {
-		$.post('/changeroom/' + rid, {title: title});
+		var deferred = $.Deferred();
+		$.post('/changeroom/' + rid, {title: title}).done(function(res) {
+			console.log(res);
+			deferred.resolve(res);
+		}).fail(function(err) {
+			console.log(err);
+			deferred.reject(err);
+		});
+		return deferred.promise();
 	}
 	function changeLimit(rid, number) {
-		$.post('/changeroom/' + rid, {number: number});
+		var deferred = $.Deferred();
+		$.post('/changeroom/' + rid, {number: number}).done(function(res) {
+			console.log(res);
+			deferred.resolve(res);
+		}).fail(function(err) {
+			console.log(err);
+			deferred.reject(err);
+		});
+		return deferred.promise();
+	}
+	function confirm(data) {
+		var deferred = $.Deferred();
+		var rid = data.rid;
+		$.post('/confirm/' + rid, {day: data.day, night: data.night}).done(function(res) {
+			console.log(res);
+			deferred.resolve(res);
+		}).fail(function(err) {
+			console.log(err);
+			deferred.reject(err);
+		});
+		return deferred.promise();
 	}
 	function createRoom(data) {
 		var deferred = $.Deferred();
 		$.get('/member/' + data.maker).done(function(res) {
+			console.log(res);
 			if (res.rows[0].roomcount >= 3) {
 				var msg = '방은 최대 세 개까지 만들 수 있습니다.';
 				deferred.reject(msg);
@@ -46,10 +83,12 @@ wwm.model = (function () {
 					deferred.resolve(res);				
 				}).fail(function (err) {
 					console.log(err);
+					deferred.reject(err);
 				});
 			}
 		}).fail(function (err) {
 			console.log(err);
+			deferred.reject(err);
 		});
 		return deferred.promise();
 	}
@@ -75,6 +114,7 @@ wwm.model = (function () {
 		ban: ban,
 		changeTitle: changeTitle,
 		changeLimit: changeLimit,
-		searchList: searchList
+		searchList: searchList,
+		confirm: confirm
 	};
 }());
