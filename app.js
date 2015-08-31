f1bad7ab7e419245778317e6922ad548971c2373
Zero Cho <zerohch0@gmail.com>
2015-08-31 15:53:44 +0900

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.engine('dust', adaro.dust());
+app.engine('dust', adaro.dust({'whitespace': true}));
 app.set('view engine', 'dust');
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
